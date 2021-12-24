158d179b2c1f753287395d2ff2789697e49725b4
Sujit Maharjan <shubhajeet.per@gmail.com>
2021-12-23 19:42:05 -0500

    adding cmake readme and test jupyter notebook

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Readme
+
+## About 
+The project serve as a header/library for generating sample data of specific distributions. Distributions currently implemented.
+1. logarithmic
+2. exponential
+3. linear
+4. power
+
+## Author
+Sujit Maharjan
--- src/samplegen.h
+++ src/samplegen.h
@@ -2,24 +2,26 @@
 #include <cmath>
 #include <functional>
 
+#define MIN_RANGE 1.0 
+#define MAX_RANGE 100.0
 template <typename T>
 std::function<T()> log_gen(T start, T stop, T base) {
-    auto bias = pow(base,1.0);
-    auto norm = pow(base,2.0) - bias;
+    auto bias = pow(base,MIN_RANGE);
+    auto norm = pow(base,MAX_RANGE) - bias;
     return [=]() {
         std::mt19937_64 gen(std::random_device{}());
-        std::uniform_real_distribution<double> dis(1.0,2.0);
+        std::uniform_real_distribution<double> dis(MIN_RANGE,MAX_RANGE);
         return T((pow(base,dis(gen))-bias)/norm*(stop-start) + start);
         };
 }
 
 template <typename T>
 std::function<T()> nlog_gen(T start, T stop) {
-    auto bias = exp(1.0);
-    auto norm = exp(2.0) - bias;
+    auto bias = exp(MIN_RANGE);
+    auto norm = exp(MAX_RANGE) - bias;
     return [=]() {
         std::mt19937_64 gen(std::random_device{}());
-        std::uniform_real_distribution<double> dis(1.0,2.0);
+        std::uniform_real_distribution<double> dis(MIN_RANGE,MAX_RANGE);
         return T((exp(dis(gen))-bias)/norm*(stop-start) + start);
         };
 }
@@ -27,11 +29,11 @@ std::function<T()> nlog_gen(T start, T stop) {
 
 template <typename T>
 std::function<T()> exp_gen(T start, T stop) {
-    auto bias = log(1.0);
-    auto norm = log(2.0) - bias;
+    auto bias = log(MIN_RANGE);
+    auto norm = log(MAX_RANGE) - bias;
     return [=]() {
         std::mt19937_64 gen(std::random_device{}());
-        std::uniform_real_distribution<double> dis(1.0,2.0);
+        std::uniform_real_distribution<double> dis(MIN_RANGE,MAX_RANGE);
         return T((log(dis(gen))-bias)/norm*(stop-start) + start);
         };
 }
@@ -39,11 +41,11 @@ std::function<T()> exp_gen(T start, T stop) {
 
 template <typename T>
 std::function<T()> power_gen(T start, T stop, T power) {
-    auto bias = log(1.0)/log(power);
-    auto norm = log(2.0)/log(power) - bias;
+    auto bias = log(MIN_RANGE)/log(power);
+    auto norm = log(MAX_RANGE)/log(power) - bias;
     return [=]() {
         std::mt19937_64 gen(std::random_device{}());
-        std::uniform_real_distribution<double> dis(1.0,2.0);
+        std::uniform_real_distribution<double> dis(MIN_RANGE,MAX_RANGE);
         return T((log(dis(gen))/log(power)-bias)/norm*(stop-start) + start);
         };
 }
@@ -53,7 +55,7 @@ template <typename T>
 std::function<T()> linear_gen(T start, T stop) {
     return [=]() {
         std::mt19937_64 gen(std::random_device{}());
-        std::uniform_real_distribution<double> dis(0.0,1.0);
+        std::uniform_real_distribution<double> dis(MIN_RANGE,MAX_RANGE);
         return T(dis(gen)*(stop-start) + start);
         };
 }
